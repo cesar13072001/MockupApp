58f44fe93c5c90154b8b5719d8c3b3e040fdd132
Cesar Aguilar <cesar13072001@gmail.com>
2023-08-03 21:18:46 -0500

    Agregar archivos de proyecto.

---
+++ MockupApp/Controllers/StoreController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MockupApp.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        public ActionResult Shopcart()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ MockupApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MockupApp.MvcApplication" Language="C#" %>
